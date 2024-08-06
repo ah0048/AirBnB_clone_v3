f25702977864d043c22c756e5643946a2047d065
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-08-07 00:03:58 +0300

    edit stat_count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 '''index file'''
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 from api.v1.views import app_views
 from flask import jsonify
@@ -18,12 +12,14 @@ def status():
     return jsonify({'status': 'OK'})
 
 
+classes = {"amenities": 'Amenity', "cities": 'City', "places": 'Place',
+           "reviews": 'Review', "states": 'State', "users": 'User'}
+
+
 @app_views.route('/stats', strict_slashes=False)
 def stat_count():
     '''count number of each objects by type'''
-    classes = {"amenities": Amenity, "cities": City, "places": Place,
-               "reviews": Review, "states": State, "users": User}
     resp = {}
     for key, value in classes.items():
-        classes[key] = storage.count(value)
-    return jsonify(classes)
+        resp[key] = storage.count(value)
+    return jsonify(resp)
